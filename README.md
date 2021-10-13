5ccf9cfbc8e84acda7483ba29c45b797dde57897
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-10-13 08:20:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# PowerJDEP
+
+A dumb small tool because im too lazy to learn how to better analyze jdeps output for now.
+
+## How to use
+Either by command line:
+```java -jar PowerJDEP.jar C:\file1.txt```
+
+## Example output
+```
+Needed packages:
+java.desktop
+java.datatransfer
+java.base
+```
---
+++ src/main/java/io/wollinger/powerjdep/PowerJDEP.java
@@ -0,0 +1,45 @@
+package io.wollinger.powerjdep;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class PowerJDEP {
+    public static void main(String[] args) {
+        if(!(args.length < 1)) {
+            File inputFile = new File(args[0]);
+            if(!inputFile.exists()) {
+                System.out.println("File does not exist.");
+                System.exit(0);
+            }
+            ArrayList<String> list = load(inputFile);
+            ArrayList<String> result = new ArrayList<>();
+            for(String data : list) {
+                if(!result.contains(data))
+                    result.add(data);
+            }
+            System.out.println("Needed packages:");
+            for(String resultData : result) {
+                System.out.println(resultData);
+            }
+        } else {
+            System.out.println("File missing!");
+        }
+    }
+
+    public static ArrayList<String> load(File file) {
+        ArrayList<String> list = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if(line.toLowerCase().startsWith(" ")) {
+                    String pckg = line.substring(108);
+                    if(!pckg.contains("PowerJDEP"))
+                        list.add(pckg);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
